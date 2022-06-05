05b0051b7bbbe858bb763b1934c0fab20c06c3ba
Mariyash1086 <milly10031986@gmail.com>
2022-06-05 22:14:22 +0400

    its HW5-2-8

--- cache.go
+++ cache.go
@@ -22,7 +22,7 @@ func NewCache() Cache {
 func (receiver Cache) Get(key string) (string, bool) {
 
 	for key1, value1 := range receiver.arr {
-		if key1 == key && value1.deadline.After(time.Now()) {
+		if key1 == key && (value1.deadline.After(time.Now()) || value1.deadline == time.Time{}) {
 			return value1.value, value1.dead
 		} else {
 			return value1.value, false
